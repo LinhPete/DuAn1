84d8135231b433baf8daeda970433c62919c56af
Linh Pete <peterdamlinh1215@gmail.com>
2024-07-23 20:04:58 +0700

    test branch

--- src/daos/SummaryDAO.java
+++ src/daos/SummaryDAO.java
@@ -17,20 +17,20 @@ public class SummaryDAO {
 
     public DefaultTableModel getTopNV() throws SQLException, ClassNotFoundException {
         String[] headers = new String[3];
-        String sql = "";
+        String sql = "{call}";
         ResultSet rs = (ResultSet) XJdbc.callNoOutput(sql);
         return XTable.creatTableModel(headers, rs, false);
     }
     
     public DefaultTableModel getTopProd() throws SQLException, ClassNotFoundException {
         String[] headers = new String[3];
-        String sql = "";
+        String sql = "{call}";
         ResultSet rs = (ResultSet) XJdbc.callNoOutput(sql);
         return XTable.creatTableModel(headers, rs, false);
     }
     
     public int getCurrentSalesCount() throws SQLException, ClassNotFoundException{
-        String sql = "call";
+        String sql = "{call}";
         return (int) XJdbc.callWithOutput(sql, 1, Types.INTEGER);
     }
 }
